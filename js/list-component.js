dcac2c531e9b66a43ca74d7d55fd265087cbbacf
Wander Tillmann <wanderlopes659@hotmail.com>
2021-04-19 00:39:26 -0300

    primeiro commit

---
+++ js/list-component.js
@@ -0,0 +1,8 @@
+Vue.component('my-list', {
+  template: `
+  <li class="poke-list-item">
+        <img :src="'https://serebii.net/pokedex-xy/icon/'+pokemon.number+'.png'" />
+        <span>{{ pokemon.number | pokeNumber }} - {{ pokemon.name }}</span>
+  </li>`,
+  props: ['pokemon']
+})
